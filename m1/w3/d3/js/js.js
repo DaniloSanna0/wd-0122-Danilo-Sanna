4155f75e69b0711ad22e7c52674fd9f6bcb709a5
DaniloSanna0 <sannadanilo98@gmail.com>
2022-04-13 22:56:53 +0200

    prova

--- m1/w3/d3/js/js.js
+++ m1/w3/d3/js/js.js
@@ -11,3 +11,23 @@ function enter(){
 let ingresso = (anni <18) ? 'ingresso vietato':'ingresso permesso';
 }
 let anni = 8
+nonsocomesifa = () => console.log("tre")
+
+function miaFunzione() {
+    console.log('hello world');
+}
+miaFunzione();
+
+const pensionamento = (compleanno, nome) => {
+    const eta = 2022 - compleanno;
+    const anniPensione = 65 - eta;
+    //return anniPensione;
+    return `${nome} va in pensione tra ${anniPensione} anni`;
+}
+console.log(pensionamento(1998,'Pinco'));
+
+const calcolaSpesa = function(conto) {
+    return conto >= 50 && conto <= 300 ? conto * 0.15 : conto * 0.2;
+}
+
+console.log(calcolaSpesa(320));
