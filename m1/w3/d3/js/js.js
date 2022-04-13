7eecabd43ac730ff1c1db75fca193137eb3df1f2
DaniloSanna0 <sannadanilo98@gmail.com>
2022-04-13 17:00:26 +0200

    function

---
+++ m1/w3/d3/js/js.js
@@ -0,0 +1,13 @@
+function calcolaAnni(compleanno) {
+    return 2022 - compleanno;
+}
+
+let compleanno = 22
+document.write(compleanno)
+
+compleanno => 2022 - compleanno
+
+function enter(){
+let ingresso = (anni <18) ? 'ingresso vietato':'ingresso permesso';
+}
+let anni = 8
