771d9c084a658f13deabcfb2f2e150d312148d2e
DaniloSanna0 <sannadanilo98@gmail.com>
2022-04-26 16:37:03 +0200

    ora

---
+++ m2/w1/d1/js/js.js
@@ -0,0 +1,22 @@
+let d = new Date()
+document.querySelector('#secondi').innerHTML = d
+
+var data = new Date(2018, 11, 24, 10, 33, 30, 7663)
+document.write(data)
+
+const r = new Date("October 13, 2014 11:13:00");
+document.write("<br>" + r)
+
+let f = Date.parse(2022)
+document.write("<br>" + f)
+
+const a = new Date(0);
+document.write("<br>" + a)
+
+document.write("<br>" + r.getMinutes())
+
+document.write("<br>" + d.getDay())
+
+document.write("<br>" + d.getHours() + ":" + d.getMinutes() + "<br>")
+
+document.write(d.toLocaleDateString('it-IT'))
